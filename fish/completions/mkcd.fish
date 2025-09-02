5ff8f8ac035b36257c52c5e45b33449094b9e2fd
Jackson Risse <j.risse14@proton.me>
2025-09-02 18:00:15 -0400

    [ADD] Improve config and mkcd fish functions with error handling and completion

---
+++ fish/completions/mkcd.fish
@@ -0,0 +1,2 @@
+# Complete with directories for mkcd command
+complete -c mkcd -d "Create and change to directory" -a "(__fish_complete_directories)"
--- fish/functions/mkcd.fish
+++ fish/functions/mkcd.fish
@@ -1,6 +1,25 @@
-function mkcd -d "Create a directory and change dir into."
-    mkdir -pv $argv
-
-    cd $argv
-
+function mkcd --description "Create a directory and change into it"
+    if test (count $argv) -eq 0
+        echo "Usage: mkcd <directory> [directories...]"
+        echo "Creates directory/directories and changes into the last one"
+        return 1
+    end
+    
+    # Store the last directory to cd into
+    set -l target_dir $argv[-1]
+    
+    # Create all directories
+    if not mkdir -pv $argv
+        echo "Failed to create directory: $argv"
+        return 1
+    end
+    
+    # Change to the last directory specified
+    if not cd $target_dir
+        echo "Failed to change to directory: $target_dir"
+        return 1
+    end
+    
+    # Show where we ended up
+    echo "Now in: $(pwd)"
 end
